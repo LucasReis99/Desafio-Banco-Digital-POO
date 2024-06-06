ac9af5ded1ff9f1b2932ef454b8200b42c8508dd
LucasReis99 <lcreis33@gmail.com>
2024-06-05 22:02:12 -0300

    Atualização

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        // Criando um cliente
+        Cliente lucas = new Cliente();
+        lucas.setNome("Lucas");
+        
+        // Criando uma conta corrente para o cliente
+        Conta cc = new ContaCorrente(lucas);
+        // Criando uma conta poupança para o cliente
+        Conta poupanca = new ContaPoupanca(lucas);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        // Realizando um depósito na conta corrente
+        cc.depositar(100);
+        // Transferindo dinheiro da conta corrente para a conta poupança
+        cc.transferir(100, poupanca);
+        
+        // Imprimindo o extrato da conta corrente
+        cc.imprimirExtrato();
+        // Imprimindo o extrato da conta poupança
+        poupanca.imprimirExtrato();
+    }
 
 }
